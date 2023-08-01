a9ac1f9665df6f05bc5c5fdea42f1a8e11906fe1
Brian Winkers <bwinkers@gmail.com>
2023-08-01 01:02:32 -0700

    Create a canvas list component

---
+++ frontend/src/components/canvas/CanvasList.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+  <ul>
+    <li><router-link :to="{ name: 'canvasPage', params: { canvasName: 'business' }}">{{ $t('canvas.business.name', 'Business') }}</router-link></li>
+    <li><router-link :to="{ name: 'canvasPage', params: { canvasName: 'coop' }}">{{ $t('canvas.coop.name', 'Co-op Ownership') }}</router-link></li>
+    <li><router-link :to="{ name: 'canvasPage', params: { canvasName: 's3' }}">{{ $t('canvas.s3.name', 'S3') }}</router-link></li>
+  </ul>
+  </div>
+</template>
+
+<script setup>
+
+
+</script>

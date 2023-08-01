278b2ba93be793925ee2cad41132e016b8459eac
Brian Winkers <bwinkers@gmail.com>
2023-08-01 01:02:56 -0700

    Make loaded canvas dynamic

--- frontend/src/pages/CanvasPage.vue
+++ frontend/src/pages/CanvasPage.vue
@@ -1,16 +1,20 @@
 <template>
-  <component :is="currentComponent" />
+  <div>
+    <CanvasList />
+    <component :is="currentComponent" />
+  </div>
 </template>
 
 <script setup>
 // Import major 3rd party modules, in rough order of precedence
-import { ref, computed } from "vue";
+import { ref, computed, watch } from "vue";
 import { useRoute} from "vue-router";
 
 // Import the individual canvas SFC module
 import BusinessCanvas from "../components/canvas/BusinessCanvas.vue";
 import CoopCanvas from "../components/canvas/CoopCanvas.vue";
 import S3Canvas from "../components/canvas/S3Canvas.vue";
+import CanvasList from "../components/canvas/CanvasList.vue";
 
 // Define a mapping of canvas names to their respective component name
 const canvases = {
@@ -21,10 +25,24 @@ const canvases = {
 
 // Create a route instance
 const route = useRoute();
-// Get the canvasName from the route params
-const canvasName = route.params.canvasName;
+
+// Create a ref for the canvasName
+const canvasName =ref();
+
+// Watch route and update canvas name as needed
+watch(
+  () => route.params.canvasName,
+  () => {
+    if (route.params.canvasName) {
+      canvasName.value = route.params.canvasName;
+    }
+  },
+  { immediate: true } // Makes it take effect on first load
+);
+
 // Use the current canvasName to find the mapped component
-const currentComponent = computed(() => canvases[canvasName])
+const currentComponent = computed(() => canvases[canvasName.value]);
+
 
 </script>
 

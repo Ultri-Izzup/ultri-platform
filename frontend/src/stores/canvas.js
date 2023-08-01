47b515f591eecc14441da0bd16845f1d6c4c837f
Brian Winkers <bwinkers@gmail.com>
2023-08-01 14:45:17 -0700

    Define mock data

--- frontend/src/stores/canvas.js
+++ frontend/src/stores/canvas.js
@@ -3,9 +3,23 @@ import { useStorage } from '@vueuse/core';
 
 export const useCanvasStore = defineStore('canvas', {
   state: () => ({
-    business: useStorage('business', new Map()),
-    coop: useStorage('coop', new Map()),
-    s3: useStorage('s3', new Map()),
+    business: useStorage('business', {
+      partners: new Map(),
+      activities: new Map(),
+      resources: new Map(),
+      valueProps: new Map(),
+      custRelations: new Map(),
+      channels: new Map(),
+      custSegments: new Map(),
+      costs: new Map(),
+      revenue: new Map()
+    }),
+    coop: useStorage('coop', {
+      // Create new maps for each canvgas section
+    }),
+    s3: useStorage('s3',{
+      // Create new maps for each canvgas section
+    }),
   }),
   getters: {},
   actions: {

5efa19c1b544690434978ab84b8ece84be3de8ad
Brian Winkers <bwinkers@gmail.com>
2023-08-01 20:23:36 -0700

    Made Canvas sidebar link to canvases

--- frontend/src/layouts/drawers/CanvasDrawer.vue
+++ frontend/src/layouts/drawers/CanvasDrawer.vue
@@ -4,63 +4,24 @@
       <q-toolbar-title>Ultri Co-op</q-toolbar-title>
     </q-toolbar>
     <q-list>
-      <q-expansion-item
-        expand-separator
-        icon="mdi-account-group"
-        :label="$t('homepageDrawer.coopOwnership.label')"
-        :caption="$t('homepageDrawer.coopOwnership.caption')"
-        group="coops"
-      >
-        <q-card>
-          <q-card-section>
-            {{ $t('homepageDrawer.coopOwnership.blurb') }}
-          </q-card-section>
-          <q-card-actions class="justify-center">
-            <q-btn  class="text-center" rounded color="primary" :label="$t('homepageDrawer.joinTheDiscussion')" href="https://discord.gg/MSY9Yhss"></q-btn>
-          </q-card-actions>
-          <q-card-actions class="justify-center">
-            <a href="https://coop.ultri.com">Watch it get made</a>
-          </q-card-actions>
-        </q-card>
-      </q-expansion-item>
-      <q-expansion-item
-        expand-separator
-        icon="mdi-account"
-        :label="$t('homepageDrawer.userOwnership.label')"
-        :caption="$t('homepageDrawer.userOwnership.caption')"
-        group="coops"
-      >
-        <q-card>
-          <q-card-section>
-            {{ $t('homepageDrawer.userOwnership.blurb') }}
-          </q-card-section>
-        </q-card>
-        <q-card-actions class="text-center justify-center">
-            <q-btn  class="text-center items-center" rounded color="primary" :label="$t('homepageDrawer.joinTheDiscussion')" href="https://discord.gg/MSY9Yhss"></q-btn>
-          </q-card-actions>
-          <q-card-actions class="justify-center">
-            <a href="https://coop.ultri.com">Watch it get made</a>
-          </q-card-actions>
-      </q-expansion-item>
-      <q-expansion-item
-        expand-separator
-        icon="mdi-code-braces"
-        :label="$t('homepageDrawer.developerOwnership.label')"
-        :caption="$t('homepageDrawer.developerOwnership.caption')"
-        group="coops"
-      >
-        <q-card>
-          <q-card-section>
-            {{ $t('homepageDrawer.developerOwnership.blurb') }}
-          </q-card-section>
-          <q-card-actions class="text-center justify-center">
-            <q-btn  class="text-center items-center" rounded color="primary" :label="$t('homepageDrawer.joinTheDiscussion')" href="https://discord.gg/MSY9Yhss"></q-btn>
-          </q-card-actions>
-          <q-card-actions class="justify-center">
-            <a href="https://coop.ultri.com">Watch it get made</a>
-          </q-card-actions>
-        </q-card>
-      </q-expansion-item>
+      <q-item clickable v-ripple to="/canvas/business">
+        <q-item-section>
+          <q-item-label>Business Canvas</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-item clickable v-ripple to="/canvas/coop">
+        <q-item-section>
+          <q-item-label>Co-op Ownership Canvas</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-item clickable v-ripple to="/canvas/s3">
+        <q-item-section>
+          <q-item-label>S3 Organization Canvas</q-item-label>
+        </q-item-section>
+      </q-item>
+
     </q-list>
   </div>
 </template>

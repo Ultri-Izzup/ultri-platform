84e2392ac8335d36263221f01eb78e0bce4d58f6
Brian Winkers <bwinkers@gmail.com>
2023-08-03 08:55:24 -0700

    WiP  cleanup reorg menu

--- frontend/src/pages/CanvasPage.vue
+++ frontend/src/pages/CanvasPage.vue
@@ -64,31 +64,6 @@ watch(
 // Use the current canvasName to find the mapped component
 const currentComponent = computed(() => canvases[canvasName.value]);
 
-// The canvases display has two modes:
-// 1. Standalone, the page doesn't have an orgId available, provides a fresh page with no data.
-// 2. Integrated, there is an orgUid in the URL and we load existing canvas data for the org.
-//    We want to lazy load the canvas data, so we load only what is needed.
-// We only need to load data when a specific canvas is defined.
-if (canvasName.value && route.params.orgUid) {
-  // Fetch the org canvas into the store
-  // canvas.loadCanvas(canvasName.value, route.params.orgUid)
-  console.log("Loading fake data for: " + route.params.orgUid);
-  const mockPartners = new Map();
-  let mok = nanoid();
-  mockPartners.set(mok, { uid: mok, text: "Start.coop" });
-  mok = nanoid();
-  mockPartners.set(mok, { uid: mok, text: "NLnet" });
-
-  const mockActivities = new Map();
-  mok = nanoid();
-  mockActivities.set(mok, { uid: mok, text: "Sell stuff" });
-  mok = nanoid();
-  mockActivities.set(mok, { uid: mok, text: "Make cool code" });
-
-  canvas.setCanvasData("business", "partners", mockPartners);
-  canvas.setCanvasData("business", "activities", mockActivities);
-}
-
 // FAB - Floating Action Button to save/download
 const fabPos = ref([18, 18]);
 const draggingFab = ref(false);

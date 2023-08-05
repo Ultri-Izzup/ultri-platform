abc8875d2190e1cf1bd8339ec45f61af098c80ed
ceimiplace <azamfiredenis@yahoo.com>
2023-08-05 14:05:19 +0300

    added data i18 for S3 Delegations Canvas

--- frontend/src/components/canvas/S3DelegationCanvas.vue
+++ frontend/src/components/canvas/S3DelegationCanvas.vue
@@ -1,9 +1,80 @@
 <template>
-  <q-page class="flex flex-center"
-    ><q-card
-      ><q-card-section class="q-pa-xl"
-        ><div class="text-h2"> S3 Delegation Canvas</div><div class="text-h2">coming soon...</div></q-card-section
-      ></q-card
-    ></q-page
-  >
+   <q-page class="container q-pa-md">
+    <CanvasSection
+      v-for="info in canvasDef"
+      :info="info"
+      canvasName="business"
+      :key="info.id"
+    />
+  </q-page>
 </template>
+<script setup>
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
+const canvasDef=[{
+    title: t("canvas.business.partners.title"),
+    instructions: t("canvas.business.partners.instructions"),
+    class: "key-partners",
+    sectionKey: "partners",
+    id: 1,
+  },{
+    title: t("canvas.business.partners.title"),
+    instructions: t("canvas.business.partners.instructions"),
+    class: "key-partners",
+    sectionKey: "partners",
+    id: 2,
+  },{
+    title: t("canvas.business.partners.title"),
+    instructions: t("canvas.business.partners.instructions"),
+    class: "key-partners",
+    sectionKey: "partners",
+    id: 3,
+  },{
+    title: t("canvas.business.partners.title"),
+    instructions: t("canvas.business.partners.instructions"),
+    class: "key-partners",
+    sectionKey: "partners",
+    id: 4,
+  },{
+    title: t("canvas.business.partners.title"),
+    instructions: t("canvas.business.partners.instructions"),
+    class: "key-partners",
+    sectionKey: "partners",
+    id: 5,
+  },{
+    title: t("canvas.business.partners.title"),
+    instructions: t("canvas.business.partners.instructions"),
+    class: "key-partners",
+    sectionKey: "partners",
+    id: 6,
+  },{
+    title: t("canvas.business.partners.title"),
+    instructions: t("canvas.business.partners.instructions"),
+    class: "key-partners",
+    sectionKey: "partners",
+    id: 7,
+  },{
+    title: t("canvas.business.partners.title"),
+    instructions: t("canvas.business.partners.instructions"),
+    class: "key-partners",
+    sectionKey: "partners",
+    id: 8,
+  },{
+    title: t("canvas.business.partners.title"),
+    instructions: t("canvas.business.partners.instructions"),
+    class: "key-partners",
+    sectionKey: "partners",
+    id: 9,
+  },{
+    title: t("canvas.business.partners.title"),
+    instructions: t("canvas.business.partners.instructions"),
+    class: "key-partners",
+    sectionKey: "partners",
+    id: 10,
+  },{
+    title: t("canvas.business.partners.title"),
+    instructions: t("canvas.business.partners.instructions"),
+    class: "key-partners",
+    sectionKey: "partners",
+    id: 11,
+  }]</script>
--- frontend/src/i18n/en-US/canvas.js
+++ frontend/src/i18n/en-US/canvas.js
@@ -1,76 +1,143 @@
 export default {
   canvas: {
-    description: 'Some of the best startup canvas ideas, in a free online version.',
+    description:
+      "Some of the best startup canvas ideas, in a free online version.",
     features: {
-
-      download: 'Save you work in an easy to use JSON format',
-      upload: 'Upload the export later to update your data',
-      soon: 'Save to your Ultri Organization (coming soon)'
+      download: "Save you work in an easy to use JSON format",
+      upload: "Upload the export later to update your data",
+      soon: "Save to your Ultri Organization (coming soon)",
     },
-    callForAdditions: 'If you know of other canvas you would like converted, contact us, we will try our best to accomodate you.',
+    callForAdditions:
+      "If you know of other canvas you would like converted, contact us, we will try our best to accomodate you.",
     business: {
-      name: 'Business Model Canvas',
-      description: 'Develop new business models or document existing ones',
+      name: "Business Model Canvas",
+      description: "Develop new business models or document existing ones",
       partners: {
-        title: 'Key Partners',
-        instructions: 'Who are your key partners or suppliers? What resources do they provide? What activities do they perform?'
+        title: "Key Partners",
+        instructions:
+          "Who are your key partners or suppliers? What resources do they provide? What activities do they perform?",
       },
       activities: {
-        title: 'Key Activities',
-        instructions: 'What activities are required to meet your value propositions, distribution channels, customer relationships and revenue streams?'
+        title: "Key Activities",
+        instructions:
+          "What activities are required to meet your value propositions, distribution channels, customer relationships and revenue streams?",
       },
       resources: {
-        title: 'Key Resources',
-        instructions: 'What resources are required to meet your value propositions, distribution channels, customer relationships and revenue streams?'
+        title: "Key Resources",
+        instructions:
+          "What resources are required to meet your value propositions, distribution channels, customer relationships and revenue streams?",
       },
       valueProps: {
-        title: 'Value Propositions',
-        instructions: 'What value do you deliver to the customer? What customer problems are you solving, or needs are you satisfying? How are you solving each?'
+        title: "Value Propositions",
+        instructions:
+          "What value do you deliver to the customer? What customer problems are you solving, or needs are you satisfying? How are you solving each?",
       },
       custRelations: {
-        title: 'Customer Relations',
-        instructions: 'What does each customer segment expect? Which relationships have been established? How are they or will they be integrated? What are the related costs?'
+        title: "Customer Relations",
+        instructions:
+          "What does each customer segment expect? Which relationships have been established? How are they or will they be integrated? What are the related costs?",
       },
       custSegments: {
-        title: 'Customer Segments',
-        instructions: 'Who are your most important customers? Who are you creating value for?'
+        title: "Customer Segments",
+        instructions:
+          "Who are your most important customers? Who are you creating value for?",
       },
       channels: {
-        title: 'Channels',
-        instructions: 'What channels are right for your customer segments? How do you reach them in the best channels? How cost effective are the segments? How will you integrate the channels?'
+        title: "Channels",
+        instructions:
+          "What channels are right for your customer segments? How do you reach them in the best channels? How cost effective are the segments? How will you integrate the channels?",
       },
       revenue: {
-        title: 'Revenue Streams',
-        instructions: 'What value do you provide that people will pay for? How much and how are they currently paying? How much does each stream contribute to revenues?'
+        title: "Revenue Streams",
+        instructions:
+          "What value do you provide that people will pay for? How much and how are they currently paying? How much does each stream contribute to revenues?",
       },
       costs: {
-        title: 'Cost Structure',
-        instructions: 'What costs are associated with key resources or activities? What are the most important costs and strategies for managing them?'
-      }
+        title: "Cost Structure",
+        instructions:
+          "What costs are associated with key resources or activities? What are the most important costs and strategies for managing them?",
+      },
     },
     coop: {
-      name: 'Co-op Ownership Canvas',
-      description: 'Understand and refine your co-op ownership plans (soon).'
+      name: "Co-op Ownership Canvas",
+      description: "Understand and refine your co-op ownership plans (soon).",
     },
     lean: {
-      name: 'Lean Canvas',
-      description: 'Brainstorm new business models or analyze existing ones (soon).'
+      name: "Lean Canvas",
+      description:
+        "Brainstorm new business models or analyze existing ones (soon).",
     },
     s3: {
-      name: 'S3 Canvas',
+      name: "S3 Canvas",
       delegation: {
-        name: 'Delegation Canvas',
-        description: 'Define the domain or boundaries of a circle or group (soon).'
+        name: "Delegation Canvas",
+        description:
+          "Define the domain or boundaries of a circle or group (soon).",
+        purpose: {
+          title: "Purpose",
+          instructions:
+            "What purpose does the team (or role) serve in the organization?",
+        },
+        responsabilities: {
+          title: "Key Responsabilities",
+          instructions:
+            "What are the delegatee’s main responsibilities, i.e. objectives, or essential work and decision making being delegated?",
+        },
+        dependencies: {
+          title: "Dependencies",
+          instructions:
+            "What are the essential dependencies between this domain and other parts of the organization?",
+        },
+        constraints: {
+          title: "External Constraints",
+          instructions:
+            "What are important external constraints to the autonomy and influence of the delegatee(s)?",
+        },
+        challenges: {
+          title: "Key Challenges",
+          instructions:
+            "What are the three most important known (or anticipated) challenges the delegatee(s) might face?",
+        },
+        deliverables: {
+          title: " Key Deliverables",
+          instructions:
+            "What does the team/role provide to achieve its purpose and meet the key responsibilities and key challenges?",
+        },
+        competencies: {
+          title: "Competencies, qualities and skills",
+          instructions:
+            "What competencies, qualities and skills are required – or at least preferable – to successfully achieve the purpose of this domain?",
+        },
+        resources: {
+          title: "Key Resources",
+          instructions:
+            "What are essential resources the delegatee(s) can make use of?",
+        },
+        delegator: {
+          title: "Delegator Responsibilities",
+          instructions:
+            "What is the delegator’s contribution to the success of the delegatee(s)?",
+        },
+        metrics: {
+          title: "Key Metrics",
+          instructions:
+            "What are the critical indicators of progress, project health or performance?",
+        },
+        monitoring: {
+          title: "Monitoring and Evaluation",
+          instructions:
+            "How will you monitor the key metrics, and when (and how) will you evaluate success of the team/role?",
+        },
       },
       organization: {
-        name: 'Organization Canvas',
-        description: 'Understand an organizations business model (soon).'
+        name: "Organization Canvas",
+        description: "Understand an organizations business model (soon).",
       },
       team: {
-        name: 'Team Canvas',
-        description: 'Define the organization with an inward perspective (soon).'
-      }
+        name: "Team Canvas",
+        description:
+          "Define the organization with an inward perspective (soon).",
+      },
     },
-
-  }
+  },
 };

bc62705c43d6be684eb3f58967f92da27714f43a
Brian Winkers <bwinkers@gmail.com>
2023-08-05 16:58:46 -0700

    draft circles store

--- frontend/src/stores/circles.js
+++ frontend/src/stores/circles.js
@@ -1,12 +1,64 @@
-import { defineStore } from "pinia";
+import { defineStore, storeToRefs  } from "pinia";
 import { useStorage } from "@vueuse/core";
+import { ref } from "vue";
+import { api } from "boot/axios";
 
-export const useCirclesStore = defineStore("circles", {
-  state: () => ({
+export const useCirclesStore = defineStore("circles", () => {
 
-  }),
-  getters: {},
-  actions: {
+  const circles = ref(useStorage("circles", new Map()));
+  const currentCircleUid = ref(useStorage("currentCircleUid", null));
+  const topCircleUid = ref(useStorage("topCircleUid", null));
+  const fetching = ref(false);
+  const showCircleDialog = ref(false);
 
-  },
+  const setCurrentCircle = (uid) => {
+    currentCircleUid.value = uid;
+  };
+
+  const triggerCircleDialog = () => {
+    showCircleDialog.value = true;
+  };
+
+  const validateCircleName = (orgName) => {
+    return orgName.length >= 2 && orgName.length <= 64;
+  }
+
+  const createCircle = async (orgUid, circle) => {
+    const result = await api.post("/orgs/" + orgUid + '/circles', { circle });
+
+    const circleData = result.data;
+
+    console.log(circleData);
+  };
+
+  const updateCircle = async (circle) => {
+    const result =  await api.put("/orgs/" + orgUid + '/circles', { circle });
+
+    const circleData = result.data;
+
+    console.log(circleData);
+  };
+
+  const $reset = () => {
+    console.log(
+    'RESET CIRCLES'
+    )
+    circles.value = new Map();
+    currentCircleUid.value = null;
+    topCircleUid.value = null;
+    fetching.value = false;
+    showCircleDialog.value = false;
+  }
+
+  return {
+    circles,
+    currentCircleUid,
+    topCircleUid,
+    fetching,
+    showCircleDialog,
+    setCurrentCircle,
+    triggerCircleDialog,
+    validateCircleName,
+    $reset
+  };
 });

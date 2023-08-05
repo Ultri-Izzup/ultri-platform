848662a878cd551cc3d96b9b5e22bf261252ee22
Brian Winkers <bwinkers@gmail.com>
2023-08-05 16:18:22 -0700

    Stubbed circles store

---
+++ frontend/src/stores/circles.js
@@ -0,0 +1,12 @@
+import { defineStore } from "pinia";
+import { useStorage } from "@vueuse/core";
+
+export const useCirclesStore = defineStore("circles", {
+  state: () => ({
+
+  }),
+  getters: {},
+  actions: {
+
+  },
+});

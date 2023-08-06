24b9e53a2298c6c559b39f6fedb6cde6374eb99b
Brian Winkers <bwinkers@gmail.com>
2023-08-06 09:52:28 -0700

    Org circles pages

--- frontend/src/pages/OrgCirclesPage.vue
+++ frontend/src/pages/OrgCirclesPage.vue
@@ -1,11 +1,87 @@
 <template>
-  <div>
-    <h2>Organization Circles</h2>
-
-  </div>
+  <q-page>
+    <div class="text-h3 text-center justify-center row full-width">
+      {{ $t("circles.dashboard.title") }}
+    </div>
+    <div class="fit">
+      <div
+        v-if="hasCircles"
+      >
+        <div class="row">
+          <div class="col-12">
+            <q-btn
+              :label="$t('circles.reset.button.text')"
+              color="primary"
+              @click="circlesStore.$reset()"
+            ></q-btn>
+            <q-tree
+              :nodes="treeData.items"
+              node-key="uid"
+              v-model:selected="selected"
+              default-expand-all
+            ></q-tree>
+          </div>
+        </div>
+      </div>
+      <div v-else>
+        <q-btn
+          :label="$t('circles.firstCircle.button.text')"
+          color="primary"
+          @click="circlesStore.initCircles()"
+        ></q-btn>
+      </div>
+    </div>
+  </q-page>
 </template>
-<script>
 
+<script setup>
+import { onMounted, computed, ref, watch } from "vue";
+
+import { useCirclesStore } from "../stores/circles";
+
+const circlesStore = useCirclesStore();
+
+const selected = ref(null);
+
+const unselectNode = () => {
+  selected.value = null;
+};
+
+const hasCircles = computed(() => {
+  return circlesStore.orgCircles.length;
+});
+
+const treeData = computed(() => {
+  const buildTree = (parentId) => (item) => {
+    const children = circlesStore.orgCircles.filter(
+      (child) => child.parentCircle === item.uid
+    );
+    return {
+      ...item,
+      ...(children.length > 0 && {
+        children: children.map(buildTree(item.uid)),
+      }),
+    };
+  };
+
+  const nestedData = {
+    items: circlesStore.orgCircles
+      .filter((item) => !item.parentCircle)
+      .map(buildTree(undefined)),
+  };
+
+  return nestedData;
+});
+
+watch(selected, () => {
+  console.log('SELECTED', selected)
+
+});
+
+// Load circle data for the org into the store
+//circlesStore.triggerCircleDialog();
+
+onMounted(() => {});
 </script>
-<style scoped>
-</style>
+
+<style lang="scss"></style>

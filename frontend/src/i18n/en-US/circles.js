8dc1634de6ef41a2d9e7111a1fa8b40f450d7578
Brian Winkers <bwinkers@gmail.com>
2023-08-06 09:53:47 -0700

    Circels i18n work

---
+++ frontend/src/i18n/en-US/circles.js
@@ -0,0 +1,17 @@
+export default {
+  circles: {
+    dashboard: {
+      title: 'Circles'
+    },
+    firstCircle: {
+      button: {
+        text: 'Start Building'
+      }
+    },
+    reset: {
+      button: {
+        text: 'Reset'
+      }
+    }
+  },
+};

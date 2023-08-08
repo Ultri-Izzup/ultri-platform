0474f4c0dcb59f82c1030b1be1a1c6d1fc07d3ed
Brian Winkers <bwinkers@gmail.com>
2023-08-07 20:48:15 -0700

    minimized duplication where I could

--- frontend/src/components/footer/UDynamicFooter.vue
+++ frontend/src/components/footer/UDynamicFooter.vue
@@ -1,51 +1,88 @@
 <template>
-  <q-footer v-if="route.name === 'canvasPage'">
+  <q-footer>
+    <q-toolbar
+      :class="colorStore.darkMode ? 'u-navbar-dark' : 'u-navbar-light'"
+    >
+    <!-- CANVAS FOOTER w/ ATTRIBUTION -->
+      <div v-if="route.name == 'canvasPage'" class="row full-width">
+        <!-- XS SCREEN -->
+        <div class="xs row full-width">
+          <div class="col q-py-sm text-caption">
+            {{ $t('canvas.'+canvasName+'.attribution', ' ')}}
+          </div>
+          <div class="col-2 q-py-sm text-right justify-right">
+            <q-space /><a
+              href="https://www.linkedin.com/company/ultri/"
+              class="navbar-text-link"
+              >LinkedIn</a
+            >
+            <q-space />
+            <div class="text-body2">&copy; Ultri 2023</div>
+          </div>
+        </div>
+        <!-- LARGER THAN XS -->
+        <div class="gt-xs row full-width">
+          <q-space /><a href="https://coop.ultri.com" class="navbar-text-link"
+            >Co-op Info</a
+          >
+          <q-space /><a
+            href="https://www.opensociocracy.org"
+            class="navbar-text-link"
+            >OpenSociocracy</a
+          >
+          <q-space /><a
+            href="https://www.linkedin.com/company/ultri/"
+            class="navbar-text-link"
+            >LinkedIn</a
+          >
+          <q-space />
+          <div class="text-body2 q-pr-md">Copyright &copy; 2023 Ultri</div>
+        </div>
+      </div>
+    <!-- STANDARD FOOTER -->
+      <div v-else class="row full-width">
+        <!-- XS SCREEN -->
+        <div class="xs row full-width">
+          <div class="col">
+            <q-space /><a href="https://coop.ultri.com" class="navbar-text-link"
+              >Co-op Info</a
+            >
+            <q-space /><a
+              href="https://www.opensociocracy.org"
+              class="navbar-text-link"
+              >OpenSociocracy</a
+            >
+          </div>
+          <div class="col text-right justify-right">
+            <q-space /><a
+              href="https://www.linkedin.com/company/ultri/"
+              class="navbar-text-link"
+              >LinkedIn</a
+            >
+            <q-space />
+            <div class="text-body2">Copyright &copy; 2023 Ultri</div>
+          </div>
+        </div>
+        <!-- LARGER THAN XS -->
+        <div class="gt-xs row full-width">
+          <q-space /><a href="https://coop.ultri.com" class="navbar-text-link"
+            >Co-op Info</a
+          >
+          <q-space /><a
+            href="https://www.opensociocracy.org"
+            class="navbar-text-link"
+            >OpenSociocracy</a
+          >
+          <q-space /><a
+            href="https://www.linkedin.com/company/ultri/"
+            class="navbar-text-link"
+            >LinkedIn</a
+          >
+          <q-space />
+          <div class="text-body2 q-pr-md">Copyright &copy; 2023 Ultri</div>
+        </div>
+      </div>
 
-  </q-footer>
-  <q-footer v-else>
-    <q-toolbar
-      :class="colorStore.darkMode ? 'u-navbar-dark xs' : 'u-navbar-light xs'"
-    >
-      <div class="col">
-        <q-space /><a href="https://coop.ultri.com" class="navbar-text-link"
-          >Co-op Info</a
-        >
-        <q-space /><a
-          href="https://www.opensociocracy.org"
-          class="navbar-text-link"
-          >OpenSociocracy</a
-        >
-      </div>
-      <div class="col text-right justify-right">
-        <q-space /><a
-          href="https://www.linkedin.com/company/ultri/"
-          class="navbar-text-link"
-          >LinkedIn</a
-        >
-        <q-space />
-        <div class="text-body2">Copyright &copy; 2023 Ultri</div>
-      </div>
-    </q-toolbar>
-    <q-toolbar
-      :class="
-        colorStore.darkMode ? 'u-navbar-dark gt-xs' : 'u-navbar-light gt-xs'
-      "
-    >
-      <q-space /><a href="https://coop.ultri.com" class="navbar-text-link"
-        >Co-op Info</a
-      >
-      <q-space /><a
-        href="https://www.opensociocracy.org"
-        class="navbar-text-link"
-        >OpenSociocracy</a
-      >
-      <q-space /><a
-        href="https://www.linkedin.com/company/ultri/"
-        class="navbar-text-link"
-        >LinkedIn</a
-      >
-      <q-space />
-      <div class="text-body2 q-pr-md">Copyright &copy; 2023 Ultri</div>
     </q-toolbar>
   </q-footer>
 </template>
@@ -57,11 +94,11 @@ import { useColorStore } from "../../stores/color";
 
 const route = useRoute();
 
+const canvasName = route.params.canvasName;
+
 // Stores
 const colorStore = useColorStore();
-
 </script>
 
 <style lang="scss">
-
 </style>

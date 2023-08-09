77a249f60fc0c43d46d1761f6bf1827c8ca5f620
Brian Winkers <bwinkers@gmail.com>
2023-08-08 17:19:27 -0700

    Broke logic show unique attribution requirements

---
+++ frontend/src/components/canvas/CanvasAttribution.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="q-pa-xs">
+    <div v-if="canvasName == 's3-delegation'" class="full-width">
+      The S3 Delegation Canvas by Bernhard Bockelbrink is licensed under a
+      <a href="https://creativecommons.org/licenses/by-sa/4.0/"
+        >Creative Commons Attribution-ShareAlike 4.0 International License</a
+      >. Changes have been made to fit the space and purpose. More information on the S3 canvas series, and versions for printing the
+      canvas in various formats can be found at <a href="http://s3canvas.sociocracy30.org">http://s3canvas.sociocracy30.org</a>
+    </div>
+    <div v-if="canvasName == 's3-team'" class="full-width">
+      The S3 Team Canvas by Bernhard Bockelbrink is licensed under a
+      <a href="https://creativecommons.org/licenses/by-sa/4.0/"
+        >Creative Commons Attribution-ShareAlike 4.0 International License</a
+      >. Changes have been made to fit the space and purpose. More information on the S3 canvas series, and versions for printing the
+      canvas in various formats can be found at <a href="http://s3canvas.sociocracy30.org">http://s3canvas.sociocracy30.org</a>
+    </div>
+    <div v-if="canvasName == 's3-organization'" class="full-width">
+      The S3 Organization Canvas by Bernhard Bockelbrink is licensed under a
+      <a href="https://creativecommons.org/licenses/by-sa/4.0/"
+        >Creative Commons Attribution-ShareAlike 4.0 International License</a
+      >. Changes have been made to fit the space and purpose. More information on the S3 canvas series, and versions for printing the
+      canvas in various formats can be found at <a href="http://s3canvas.sociocracy30.org">http://s3canvas.sociocracy30.org</a>
+    </div>
+    <div v-if="canvasName == 'lean'" class="full-width">
+
+    </div>
+    <div v-if="canvasName == 'coop'" class="full-width">
+
+    </div>
+    <div v-if="canvasName == 'business'" class="full-width">
+      {{ $t('canvas.business.attribution')}}
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+const route = useRoute();
+const canvasName = computed(() => route.params.canvasName);
+</script>

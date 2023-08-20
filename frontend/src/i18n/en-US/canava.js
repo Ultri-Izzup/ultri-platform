b601f7fcde30c1098d11f7dfea5a35af468ac9a1
Brian Winkers <bwinkers@gmail.com>
2023-08-20 13:30:39 -0700

    Initial Canava language file

---
+++ frontend/src/i18n/en-US/canava.js
@@ -0,0 +1,21 @@
+export default {
+  canava: {
+    product: {
+      name: 'Canava',
+      description: 'Business Canvas Editor',
+    },
+    page: {
+      title: "Canava - Custom Business Canvas Editor",
+      description: "Customize over 20 popular canvas or create one from scratch."
+
+    },
+    component: {
+      editor: {
+        title: "Canava - Canvas Editor"
+      },
+      renderer: {
+        title: "Canava - Canvas Display"
+      }
+    }
+  }
+}

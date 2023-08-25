dfde7bb63da258e737f03f6dbf891b4cc251eb13
Brian Winkers <bwinkers@gmail.com>
2023-08-25 15:48:00 -0700

    Good enough "save as image"

--- frontend/src/components/canava/CanavaEditor.vue
+++ frontend/src/components/canava/CanavaEditor.vue
@@ -7,6 +7,18 @@
             <q-slider v-model="vh" :min="50" :max="200" :step="1"></q-slider>
           </div>
           <div class="col-1 justify-left q-pl-md q-pt-xs">Stretch</div>
+          <div class="col-2 justify-right q-pl-xl">
+            <q-btn
+              @click="takeSnapshot()"
+              color="primary"
+              dense
+              flat
+              icon="mdi-camera"
+              class="q-pt-none"
+              label="Snapshot"
+              ><q-tooltip>Download Image</q-tooltip></q-btn
+            >
+          </div>
         </div>
       </div>
       <div :class="Screen.gt.md ? ' col-6 ' : ' col-12 '">
@@ -40,156 +52,168 @@
     </div>
 
     <q-separator />
-    <div class="row q-pa-md">
-      <span class="text-h3">{{ value.name }}</span>
-      <q-space></q-space>
-    </div>
-    <div class="q-ma-sm container-wrapper">
-      <div class="container">
-        <q-card
-          class="container-card"
-          :class="section.sectionKey"
-          v-for="section in value.sections"
-          :key="section.sectionKey"
-          :style="
-            'background-color: ' +
-            section.backgroundColor +
-            '; color: ' +
-            section.textColor
-          "
-        >
-          <q-card-section
-            :class="colorStore.darkMode ? 'dark-top-q-card' : 'top-q-card'"
-            :style="'background-color: ' + section.backgroundColor"
+    <div id="canvas-image-wrapper">
+      <div class="row q-pa-md">
+        <span class="text-h3">{{ value.name }}</span>
+        <q-space></q-space>
+      </div>
+      <div class="q-ma-sm container-wrapper">
+        <div class="container">
+          <q-card
+            class="container-card"
+            :class="section.sectionKey"
+            v-for="section in value.sections"
+            :key="section.sectionKey"
+            :style="
+              'background-color: ' +
+              section.backgroundColor +
+              '; color: ' +
+              section.textColor
+            "
           >
-            <p class="text-bold">
-              <q-avatar v-if="section.sequence" size="24px" color="grey-4"
-                ><span class="text-grey-8">{{
-                  section.sequence
-                }}</span></q-avatar
-              >
-              {{ section.title }}
-              <sup>
-                <q-icon name="mdi-help-circle" size="13px" color="grey-6" />
-                <q-tooltip>
-                  <span v-html="section.instructions" />
-                </q-tooltip>
-              </sup>
-            </p>
-            <q-icon
-              name="mdi-plus-circle-outline"
-              size="20px"
-              clickable
-              v-ripple
-              @click="triggerNew(section.sectionKey)"
-              class="cursor-pointer"
-            ></q-icon>
-          </q-card-section>
-          <q-card-section
-            v-if="showInstructions && !section.items"
-            v-ripple
-            clickable
-            @click="triggerNew(section.sectionKey)"
-            class="q-pa-md text-italic cursor-pointer q-hoverable"
-            ><div v-html="section.instructions" />
-          </q-card-section>
-          <q-card-section v-if="section.items && showItems" class="q-pa-none">
-            <q-list separator>
-              <q-item v-for="item in section.items" :key="item.uid">
-                <q-item-section>
-                  <q-item-label class="text-bold">
-                    {{ item.label }}
-                  </q-item-label>
-                  <span
-                    v-if="showDetail"
-                    v-html="item.details"
-                    clickable
-                    v-ripple
-                    @click="editItem(section.sectionKey, item.uid)"
-                  ></span>
-                </q-item-section>
-              </q-item>
-            </q-list>
-          </q-card-section>
-        </q-card>
-        <q-dialog v-model="showDialog">
-          <q-card>
-            <q-bar class="bg-primary">
-              {{ currentSectionData.title }} Item
-              <sup
-                class="q-pl-xs clickable v-ripple"
-                @click="toggleEditorHelp()"
-              >
-                <q-icon name="mdi-help-circle" size="13px" />
-              </sup>
-              <q-space></q-space>
-              <q-btn dense flat icon="mdi-close" v-close-popup @click="reset()">
-                <q-tooltip>{{ $t("nav.close") }} </q-tooltip>
-              </q-btn>
-            </q-bar>
-            <q-card-section>
-              <div
-                v-if="!editorHelp"
-                class="row full-width text-italic text-body1 q-pa-sm"
-              >
-                <span v-html="currentSectionData.instructions" />
-              </div>
-
-              <q-input label="Label" v-model="currentItem.label"></q-input>
-              <div class="q-pt-sm q-pb-xs text-caption text-grey-8">
-                Details
-              </div>
-              <q-editor
-                v-model="currentItem.details"
-                min-height="5rem"
-                :dense="$q.screen.lt.md"
-                :toolbar="[
-                  ['bold', 'italic', 'subscript', 'superscript'],
-                  [
-                    {
-                      icon: 'mdi-format-size',
-                      fixedLabel: true,
-                      fixedIcon: true,
-                      list: 'no-icons',
-                      options: [
-                        'size-1',
-                        'size-2',
-                        'size-3',
-                        'size-4',
-                        'size-5',
-                      ],
-                    },
-                  ],
-                  ['hr', 'link'],
-                  ['quote', 'unordered', 'ordered', 'outdent', 'indent'],
-                ]"
-              ></q-editor>
-              <!-- <q-input label="Details" v-model="currentItem.details"></q-input> -->
+            <q-card-section
+              :class="colorStore.darkMode ? 'dark-top-q-card' : 'top-q-card'"
+              :style="'background-color: ' + section.backgroundColor"
+            >
+              <p class="text-bold">
+                <q-avatar v-if="section.sequence" size="24px" color="grey-4"
+                  ><span class="text-grey-8">{{
+                    section.sequence
+                  }}</span></q-avatar
+                >
+                {{ section.title }}
+                <sup>
+                  <q-icon name="mdi-help-circle" size="13px" color="grey-6" />
+                  <q-tooltip>
+                    <span v-html="section.instructions" />
+                  </q-tooltip>
+                </sup>
+              </p>
+              <q-icon
+                name="mdi-plus-circle-outline"
+                size="20px"
+                clickable
+                v-ripple
+                @click="triggerNew(section.sectionKey)"
+                class="cursor-pointer"
+              ></q-icon>
             </q-card-section>
-            <q-card-actions class="justify-center">
-              <q-btn
-                v-if="currentItem.uid"
-                label="Delete"
-                color="secondary"
-                @click="deleteItem()"
-              ></q-btn>
-              <q-btn label="Save" color="primary" @click="saveItem()"></q-btn>
-            </q-card-actions>
+            <q-card-section
+              v-if="showInstructions && !section.items"
+              v-ripple
+              clickable
+              @click="triggerNew(section.sectionKey)"
+              class="q-pa-md text-italic cursor-pointer q-hoverable"
+              ><div v-html="section.instructions" />
+            </q-card-section>
+            <q-card-section v-if="section.items && showItems" class="q-pa-none">
+              <q-list separator>
+                <q-item v-for="item in section.items" :key="item.uid">
+                  <q-item-section>
+                    <q-item-label class="text-bold">
+                      {{ item.label }}
+                    </q-item-label>
+                    <span
+                      v-if="showDetail"
+                      v-html="item.details"
+                      clickable
+                      v-ripple
+                      @click="editItem(section.sectionKey, item.uid)"
+                    ></span>
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </q-card-section>
           </q-card>
-        </q-dialog>
+          <q-dialog v-model="showDialog">
+            <q-card>
+              <q-bar class="bg-primary">
+                {{ currentSectionData.title }} Item
+                <sup
+                  class="q-pl-xs clickable v-ripple"
+                  @click="toggleEditorHelp()"
+                >
+                  <q-icon name="mdi-help-circle" size="13px" />
+                </sup>
+                <q-space></q-space>
+                <q-btn
+                  dense
+                  flat
+                  icon="mdi-close"
+                  v-close-popup
+                  @click="reset()"
+                >
+                  <q-tooltip>{{ $t("nav.close") }} </q-tooltip>
+                </q-btn>
+              </q-bar>
+              <q-card-section>
+                <div
+                  v-if="!editorHelp"
+                  class="row full-width text-italic text-body1 q-pa-sm"
+                >
+                  <span v-html="currentSectionData.instructions" />
+                </div>
+
+                <q-input label="Label" v-model="currentItem.label"></q-input>
+                <div class="q-pt-sm q-pb-xs text-caption text-grey-8">
+                  Details
+                </div>
+                <q-editor
+                  v-model="currentItem.details"
+                  min-height="5rem"
+                  :dense="$q.screen.lt.md"
+                  :toolbar="[
+                    ['bold', 'italic', 'subscript', 'superscript'],
+                    [
+                      {
+                        icon: 'mdi-format-size',
+                        fixedLabel: true,
+                        fixedIcon: true,
+                        list: 'no-icons',
+                        options: [
+                          'size-1',
+                          'size-2',
+                          'size-3',
+                          'size-4',
+                          'size-5',
+                        ],
+                      },
+                    ],
+                    ['hr', 'link'],
+                    ['quote', 'unordered', 'ordered', 'outdent', 'indent'],
+                  ]"
+                ></q-editor>
+                <!-- <q-input label="Details" v-model="currentItem.details"></q-input> -->
+              </q-card-section>
+              <q-card-actions class="justify-center">
+                <q-btn
+                  v-if="currentItem.uid"
+                  label="Delete"
+                  color="secondary"
+                  @click="deleteItem()"
+                ></q-btn>
+                <q-btn label="Save" color="primary" @click="saveItem()"></q-btn>
+              </q-card-actions>
+            </q-card>
+          </q-dialog>
+        </div>
+      </div>
+      <div class="row full-width text-center q-pa-sm">
+        <div class="col-12 q-pa-lg" v-html="value.attribution"></div>
       </div>
     </div>
-    <div class="row full-width text-center q-pa-sm">
-      <div class="col-12 q-pa-lg" v-html="value.attribution" ></div>
-    </div>
+    <a id="downloadAnchorElem" style="display: none"></a>
   </div>
 </template>
 
 <script setup>
-import { ref, computed, watch, unref } from "vue";
+import { ref, computed, watch, unref, onMounted } from "vue";
 import { Screen } from "quasar";
 
 import { nanoid } from "nanoid";
+
+import * as htmlToImage from "html-to-image";
+import { toPng, toJpeg, toBlob, toPixelData, toSvg } from "html-to-image";
 
 import { useColorStore } from "../../stores/color";
 // Instantiate our stores early so they are available
@@ -197,6 +221,42 @@ const colorStore = useColorStore();
 
 const props = defineProps(["modelValue"]);
 const emit = defineEmits(["update:modelValue"]);
+
+// onMounted(() => {
+//   var canvasImageNode = document.getElementById("canvas-image-wrapper");
+
+// });
+
+const takeSnapshot = () => {
+  var canvasImageNode = document.getElementById("canvas-image-wrapper");
+
+  htmlToImage
+    .toPng(canvasImageNode, {
+      style: {
+        overflow: "hidden",
+        "overflow-y": "hidden",
+        "overflow-x": "hidden",
+        "::-webkit-scrollbar": "none",
+        "-ms-overflow-style": "none",
+        "scrollbar-width": "none"
+      },
+      backgroundColor: "#FFFFFF",
+    })
+    .then(function (dataUrl) {
+      console.log(dataUrl);
+
+      //   var dataStr =
+      // "data:text/json;charset=utf-8," +
+      // encodeURIComponent(JSON.stringify(outObj, 0, 2));
+      var dlAnchorElem = document.getElementById("downloadAnchorElem");
+      dlAnchorElem.setAttribute("href", dataUrl);
+      dlAnchorElem.setAttribute("download", "canvas-image.png");
+      dlAnchorElem.click();
+    })
+    .catch(function (error) {
+      console.error("oops, something went wrong!", error);
+    });
+};
 
 const currentSectionKey = ref(null);
 const currentSectionData = ref({ title: "", instructions: "" });

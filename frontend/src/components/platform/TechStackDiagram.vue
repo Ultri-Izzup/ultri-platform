5c4671d379bb18769f817dabd66250b4b646033e
Brian Winkers <bwinkers@gmail.com>
2023-08-30 13:05:04 -0700

    Stub for tech stack

---
+++ frontend/src/components/platform/TechStackDiagram.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex fit">
+    <div class="fit">
+      <vue3-mermaid
+        :nodes="data"
+        type="flowchart"
+        @nodeClick="editNode"
+      ></vue3-mermaid>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const data = [
+  {
+    id: "frontend",
+    text: "",
+    next: ["general"],
+    link: "-- Brian --->",
+  },
+  { id: "general", text: "General", edgeType: "circle", next: ["mission"], link: "-. Gail .->" },
+  { id: "general", text: "General", edgeType: "circle", next: ["product"], link: "-- Tom --->" },
+  { id: "general", text: "General", edgeType: "circle", next: ["tech"], link: "-- Geoff --->" },
+  { id: "product", text: "Product", edgeType: "circle", next: ["general"], link: "-. Ted .->" },
+  { id: "product", text: "Product", edgeType: "circle", },
+  { id: "tech", text: "Tech", edgeType: "circle", next: ["general"], link: "-. Rae .->" },
+  { id: "tech", text: "Tech", edgeType: "circle" },
+  // { id: "3", text: "C", link:"----", next: ["4", "6"] },
+  // { id: "4", text: "D", link: "-- This is the text ---", next: ["5"] },
+  // { id: "5", text: "E" },
+  // { id: "6", text: "F" },
+];
+
+const editNode = (nodeId) => {
+  alert(nodeId);
+};
+</script>
+<style></style>

52fc716cbb40ca0ee2e70e3a92213428ed0a4598
Brian Winkers <bwinkers@gmail.com>
2023-09-12 21:54:34 -0700

    Remove unused plug init

--- api/app.js
+++ api/app.js
@@ -6,7 +6,6 @@ import { dirname, join } from 'path'
 import funcs from "./plugins/funcs.js";
 
 import config from "./plugins/config.js";
-import postgres from "./plugins/postgres.js";
 import auth from "./plugins/auth.js";
 import swagger from "./plugins/swagger.js";
 
@@ -20,7 +19,6 @@ export default async function appFramework() {
   fastify.register(fastifyWebsocket);
   fastify.register(funcs);
   await fastify.register(config);
-  fastify.register(postgres);
   fastify.register(auth);
   fastify.register(swagger);
 

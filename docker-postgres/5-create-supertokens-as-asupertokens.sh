d431e12b5e42c04dcaf6d27153c3bf54e2f105df
Brian Winkers <bwinkers@gmail.com>
2023-09-12 21:53:34 -0700

    Remove old API SQL files

--- docker-postgres/5-create-supertokens-as-asupertokens.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-export PGPASSWORD=$SUPERTOKENS_PASS;
-psql -v ON_ERROR_STOP=1 --username "$SUPERTOKENS_USER"  --dbname "$DB_NAME" < /sql/supertokens-schema.sql
--- docker-postgres/8-insert-records.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-export PGPASSWORD=$API_PASS;
-psql -v ON_ERROR_STOP=1 --username "$API_USER"  --dbname "$DB_NAME" < /sql/tenants.sql
-psql -v ON_ERROR_STOP=1 --username "$API_USER"  --dbname "$DB_NAME" < /sql/nugget-schema-types.sql
